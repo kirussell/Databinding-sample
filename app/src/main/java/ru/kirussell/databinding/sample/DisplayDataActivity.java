85e9a0a36dc2500fa90a6bd7c7610ffa89d230ae
russellkim <russellkim@yandex-team.ru>
2015-11-10 19:01:54 +0300

    - initial project setup

---
+++ app/src/main/java/ru/kirussell/databinding/sample/DisplayDataActivity.java
@@ -0,0 +1,13 @@
+package ru.kirussell.databinding.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_data);
+    }
+}
